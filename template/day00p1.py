83937fc871fc6e7ad6df3dcbc1261889f911b110
minamichimaa <minamichimaa@gmail.com>
2025-12-16 14:00:28 -0500

    updated starting template for better practices

--- template/day00p1.py
+++ template/day00p1.py
@@ -2,9 +2,14 @@ def prettyPrint(array: list[str]):
     for i in array:
         print(i.strip())
 
+def main() -> int:
+    ## input
+    with open("test0.txt", "r") as f:
+        textIn = f.readlines()
 
-## input
-with open("test.txt", "r") as f:
-    textIn = f.readlines()
+    prettyPrint(textIn)
+    
+    return 
 
-prettyPrint(textIn)
+if __name__ == '__main__':
+    print(f'The answer is: {main()}')
